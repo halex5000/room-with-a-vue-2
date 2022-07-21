1f02b1488fbec0c6f68ad714896fe506213b60fc
Alex Hardman <alexhardman@launchdarkly.com>
2022-07-21 12:50:42 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Dark Launching a Room With a Vue',
+  meta: [
+    { name: 'description', content: 'Taking the LaunckDarkly Vue SDK for a spin' },
+    {
+      name: 'theme-color',
+      content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: computed(() => preferredDark.value ? '/favicon.png' : '/favicon.png'),
+    },
+  ],
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
